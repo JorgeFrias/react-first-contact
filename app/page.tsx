e25020d42760ecb1cbe8ee9a45e442b61bb1f1f5
Jorge Frias <jorge@reliby.com>
2023-07-06 03:22:08 +0200

    A bunch of stupidity.

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,16 @@ import GraphProgressBar from "../src/components/GraphProgressBarComponent";
 import Panel from "../src/components/PanelComponent";
 
 export default function Page() {
-  const handleClick = () => {
-    console.log("Button clicked!");
+  const handleClickHire = () => {
+    // Send an email to the candidate
+    const subject = "Seems like you learned React";
+    window.location.href = `mailto:me@jorgefrias.eu?subject=${encodeURIComponent(
+      subject
+    )}`;
+  };
+
+  const handleClickNotInterested = () => {
+    alert("Shame on you! Just kidding, was fun to meet you, and invest a couple of hours reading react documentation!");
   };
 
   return (
@@ -20,18 +28,18 @@ export default function Page() {
         actionGroup={[
           <CustomButton
             role={ButtonRole.Secondary}
-            onClick={handleClick}
-            children="Click me!"
+            onClick={handleClickNotInterested}
+            children="Not Interested"
           />,
           <CustomButton
             role={ButtonRole.Primary}
-            onClick={handleClick}
-            children="Better me"
+            onClick={handleClickHire}
+            children="Hire me!"
           />,
         ]}
       >
         <h1>Jorge Frías - React Report</h1>
-        <GraphProgressBar label="React experience" progress={0.1} />
+        <GraphProgressBar label="React experience" progress={0.05} />
         <GraphProgressBar label="React potential" progress={0.9} />
         <GraphProgressBar label="Overall fit" progress={1} />
       </Panel>
